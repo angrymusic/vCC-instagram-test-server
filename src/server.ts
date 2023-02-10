3f8b8b3b55cb796a3df7a7d42cfb9e826cb3dc4b
angrymusic <angrymusic@naver.com>
2023-02-10 17:40:56 +0900

    formData 처리중 Multer??

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,28 @@
 import express from "express";
 import sequelzie from "./models/index";
+const multer = require("multer");
 import { Users } from "./models/user";
+const cors = require("cors");
 const app = express();
 const port = 8090;
-
+let corsOptions = {
+    origin: "*", // 출처 허용 옵션
+    credential: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+};
+app.use(express.json()); //j
+app.use(cors(corsOptions));
 // Users.create({title:"title1",context:"context1",img:"img1"});
 
+
+
+app.use("/upload", express.static("upload"));
 app.get("/", (req, res) => {
-    
+    const list = Users.findAll();
+    console.log(list);
+});
+app.post("/add",  (req, res) => {
+    console.log(req.body.file);
+    res.json(true);
 });
 app.listen(port, async () => {
     console.log("server is running on " + port);
