6612a799e70dc9bfb5573a88d045d5fc08711ad3
angrymusic <angrymusic@naver.com>
2023-02-13 13:32:56 +0900

    사진받아서 저장, 클라이언트와 통신 완료

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,18 +1,23 @@
 import sequelize from "./index";
 import { Model, DataTypes } from "sequelize";
 interface UsersAttributes {
-    // id: number | null;
+    name: string;
     title: string;
     context: string;
-    img: Blob;
+    img: string;
 }
 export class Users extends Model<UsersAttributes> {
+    public name!: string;
     public title!: string;
     public context!: string;
-    public img!: Blob;
+    public img!: string;
 }
 Users.init(
     {
+        name: {
+            type: DataTypes.STRING(60),
+            allowNull: false,
+        },
         title: {
             type: DataTypes.STRING(60),
             allowNull: false,
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import sequelzie from "./models/index";
-const multer = require("multer");
+import multer from "multer";
 import { Users } from "./models/user";
 const cors = require("cors");
 const app = express();
@@ -11,17 +11,62 @@ let corsOptions = {
 };
 app.use(express.json()); //j
 app.use(cors(corsOptions));
+interface dataType {
+    title: string
+    context: string
+    img: string
+  }
 // Users.create({title:"title1",context:"context1",img:"img1"});
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, __dirname + "/../upload"); // 파일 업로드 경로
+    },
+    filename: function (req, file, cb) {
+        const date = new Date();
+        let month = date.getMonth() + 1;
+        let day = date.getDate();
+        let hour = date.getHours();
+        let minute = date.getMinutes();
+        let second = date.getSeconds();
 
+        let timeString: string =
+            "" +
+            date.getFullYear() +
+            (month >= 10 ? month : "0" + month) +
+            (day >= 10 ? day : "0" + day) +
+            (hour >= 10 ? hour : "0" + hour) +
+            (minute >= 10 ? minute : "0" + minute) +
+            (second >= 10 ? second : "0" + second);
+
+        const fileName = req.body.name + "-" + timeString + "-" + file.originalname;
+        cb(null, fileName); //파일 이름 설정
+        Users.create({ name: req.body.name, title: req.body.title, context: req.body.context, img: fileName });
+    },
+});
+const fileFilter = (req: any, file: any, cb: any) => {
+    // mime type 체크하여 원하는 타입만 필터링
+
+    if (file.mimetype == "image/png") {
+        cb(null, true);
+    } else {
+        cb({ msg: "png 파일만 업로드 가능합니다." }, false);
+    }
+};
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 app.use("/upload", express.static("upload"));
-app.get("/", (req, res) => {
-    const list = Users.findAll();
-    console.log(list);
+app.get("/", async (req, res) => {
+    let imgs: dataType[] = [];
+    const list = Users.findAll({ where: { name: "angrymusic" } });
+    await list.then((res) => {
+        for (let i = 0; i < res.length; i++) {
+            imgs.push(res[i].dataValues);
+        }
+    });
+    res.send(imgs);
 });
-app.post("/add",  (req, res) => {
-    console.log(req.body.file);
+app.post("/add", upload.single("file"), (req, res, next) => {
     res.json(true);
 });
 app.listen(port, async () => {
